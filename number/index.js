82cbe4c9f56cda1b6c07a8c3c4299e55ccaa1afa
Vicki7777777 <43415417+Vicki7777777@users.noreply.github.com>
2020-06-21 13:38:02 +0800

    JS-loop
    JS-loop

---
+++ number/index.js
@@ -0,0 +1,12 @@
+var i;
+for(i=1;i<=20;i++){
+	if(i%2 == 1){
+			document.write(i ," ","is odd number.");
+			document.write("<br>");
+	}else
+	{
+		document.write(i ," ","is even number.");
+		document.write("<br>");
+	}
+
+}
